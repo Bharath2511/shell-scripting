4ab07eee185cb6e4f6bdc4a2378662f0dde2b134
V S Bharath <bharathcse5t6@gmail.com>
2018-11-01 19:57:29 +0530

    user variables

--- hello.sh
+++ hello.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 echo "Hello World"
 
-echo $BASH #what is our bash shell name
-echo $BASH_VERSION #bash version
-echo $HOME #home directory
-echo $PWD #present working directory
+echo our shell name $BASH #what is our bash shell name
+echo our shell version is $BASH_VERSION #bash version
+echo our home directory is $HOME #home directory
+echo our current pwd is $PWD #present working directory
+
+#user defined variables
+
+name=Bharath
+echo The name is $name #variable can be used with $symbol before it
+
+10val=10
