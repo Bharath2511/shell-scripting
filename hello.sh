67604005c52bb42a95d641fede662376305e3292
V S Bharath <bharathcse5t6@gmail.com>
2018-11-01 20:18:25 +0530

    reading files and hiding

--- hello.sh
+++ hello.sh
@@ -11,4 +11,16 @@ echo our current pwd is $PWD #present working directory
 name=Bharath
 echo The name is $name #variable can be used with $symbol before it
 
-10val=10
+#input from keyboard
+
+# echo "name :"
+# read name1 name2 name3
+# echo "names : $name1 $name2 $name3"
+# echo $name1 #first
+
+#for echoing on same line
+ 
+read -p "username: " user_var
+read -sp "password: " pass_var
+echo "username : $user_var"
+echo "password : $pass_var"
