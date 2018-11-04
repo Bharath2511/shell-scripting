628c6cd7108f77e8fc624ba59f13e19607c5d40f
V S Bharath <bharathcse5t6@gmail.com>
2018-11-04 16:59:27 +0530

    functions

--- hello.sh
+++ hello.sh
@@ -295,21 +295,37 @@
 # done
 
 #break and continue
-for (( i=1; i<=10; i++ ))
-do
- if [ $i -gt 5 ]
- then
-  break
- fi
- echo $i
-done
+# for (( i=1; i<=10; i++ ))
+# do
+#  if [ $i -gt 5 ]
+#  then
+#   break
+#  fi
+#  echo $i
+# done
 
-#continue
-for (( i=2; i<=11; i++ ))
-do
- if [ $i -eq 3 -o $i -eq 6 ]
- then
-  continue
- fi
- echo $i
-done
+# #continue
+# for (( i=2; i<=11; i++ ))
+# do
+#  if [ $i -eq 3 -o $i -eq 6 ]
+#  then
+#   continue
+#  fi
+#  echo $i
+# done
+
+#functions
+function hello() {
+    echo "hello world"
+}
+
+quit() {
+    exit
+}
+
+hello
+
+echo "foo"
+
+quit
+
